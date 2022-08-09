16241054bcd3cb8d9f0bc10f8fa18e2598cf28f7
Nidhich16 <93552576+Nidhich16@users.noreply.github.com>
2022-08-09 12:04:44 +0530

    Add files via upload
    AppLand

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './Component/Header';
+import Feature from './Component/Feature';
+import About from './Component/About';
+import Presentation from './Component/presentation';
+import aboutimage from './images/Frame 19.png';
+import aboutimage1 from './images/download.png';
+import Contact from './Component/Contact';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+
+                                                                                            I
+     <Header/>
+     <Feature/>
+     <About image={aboutimage} title='comes with all you need for daily life' button='get the app'/>
+     <Presentation/>
+     <About img= {aboutimage1} title='Download And Get The App Now ' button ='Download'/>
+     <Contact/>
+
+     </div>
+  );
+  }
+  export default App;
